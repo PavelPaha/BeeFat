7757435b6914836c19c04347a75cf0b2e7a70e3d
AIM1rage <michael050100@gmail.com>
2023-11-13 05:31:49 +0500

    add .gitignore

--- BeeFat.App/Program.cs
+++ BeeFat.App/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
