a362abac837fe37cbff748693c7af47a246a7f02
Pavel Vasiliev <vasilievpavel4@gmail.com>
2024-01-07 15:19:48 +0500

    Explicit Test Adding track to db

--- BeeFat/Tests/Item.cs
+++
@@ -1,9 +0,0 @@
-namespace BeeFat.Tests;
-
-public class Item
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Bgu { get; set; }
-    public string Kcal { get; set; }
-}
